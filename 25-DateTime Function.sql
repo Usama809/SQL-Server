83056e04d3c831fcaff4943c40f87cdcb893133f
Usama Bin Amjad <62556162+Usama809@users.noreply.github.com>
2020-04-18 02:30:23 +0500

    25-Date Time Function

---
+++ 25-DateTime Function.sql
@@ -0,0 +1,8 @@
+--UTC stands for Corrdinated Universal Time, based on which ,the world
+--regulates clock and time .These are Slight Difference Between GMT and UTC,but the most common purpose,UTC is synonymous with GMT
+
+SELECT GETDATE(),'GATEDATE()'
+SELECT SYSDATETIMEOFFSET(),'SYSDATETIMEOFFSET'
+--SELECT CURRENT_TIMESTAMP(),'CURRENT_TIMESTAMP()'
+SELECT GETUTCDATE(),'GETUTCDATE()'
+SELECT SYSUTCDATETIME(),'SYSUTCDATETIME()'
