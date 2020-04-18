158009c8bda625fe8c75d10b9858cd6f54099e7f
Usama Bin Amjad <62556162+Usama809@users.noreply.github.com>
2020-04-18 20:33:52 +0500

    53-2NF And 3NF

---
+++ 53-2NF and 3NF.sql
@@ -0,0 +1,13 @@
+
+
+Select * from tbIEmployee5
+         --Second Normal Form (2NF)
+--A table is said to be	in 2NF, if
+--1) The Table Meet All the Condition of 1NF
+--2)Move Redundant table to a seprate table
+--3) create realtionship between these tables using foreign keys.
+       -- Third Normal Form (3NF)
+-- A table is said to be in 3NF, if the table
+--1) Meets All the condition off 1NF and 2NF
+--3) Does not contain columns(attributes) that are not fully dependent upon the primary key
+
